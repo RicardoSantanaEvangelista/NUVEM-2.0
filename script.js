d5650d898ff236aced4971153cfb64d735adb390
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2023-09-18 15:41:35 -0300

    Commit
    Number 5

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+function randomText(){
+    var text = ("!@#$%^&*()_+");
+    letters = text[Math.floor(Math.random() * text.length)];
+    return letters;
+}
+
 function rain(){
     let cloud = document.querySelector('.cloud');
     let e = document.createElement('div');
     e.classList.add('drop');
     cloud.appendChild(e);
 
-    let left = Math.floor(Math.random() * 300);
-    e.innerText = 'A';
+    let left = Math.floor(Math.random() * 290);
+    let size = Math.random() * 1.5;
+    let duration = Math.random() * 1;
+
+    e.innerText = randomText();
     e.style.left = left + 'px';
+    e.style.fontSize = 0.5+size+'em';
+    e.style.animationDuration = 1+duration+'s';
 
     setTimeout(function(){
         cloud.removeChild(e)
